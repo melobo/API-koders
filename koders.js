63883b005cb14a72c8ade23405870ea213cad4bd
melobo <warchocobo@gmail.com>
2020-02-05 18:30:07 -0600

    First commit

---
+++ koders.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Adan'
+    },
+    {
+        id: 2,
+        name: 'Cesar'
+    },
+    {
+        id: 3,
+        name: 'Itiel'
+    },
+    {
+        id: 4,
+        name: 'Ivan'
+    },
+    {
+        id: 5,
+        name: 'Jorge'
+    },
+    {
+        id: 6,
+        name: 'Manuel'
+    },
+    {
+        id: 7,
+        name: 'Miguel'
+    },
+    {
+        id: 8,
+        name: 'Octavio'
+    },
+    {
+        id: 9,
+        name: 'Oscar'
+    }
+]
